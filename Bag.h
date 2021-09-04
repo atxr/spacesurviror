602b175330ea51d283a7a26843b9d25be5841779
aTxr <alexandre.tullot@gmail.com>
2021-09-04 20:37:23 +0200

    Creer operateur + pour Bag

--- Bag.h
+++ Bag.h
@@ -5,13 +5,24 @@
 
 class Bag {
 public:
+  Bag();
   Bag(std::string type);
+
+  // Definition de l'operateur + pour la classe Bag
+  Bag operator+(const Bag &b) {
+    Bag bag;
+    bag.wood = this->wood + b.wood;
+    bag.wood = this->food + b.food;
+    bag.wood = this->iron + b.iron;
+    bag.wood = this->rock + b.rock;
+    bag.wood = this->copper + b.copper;
+    bag.wood = this->oil + b.oil;
+    return bag;
+  }
 
 private:
   int wood, food, water;
   int rock, iron, copper, oil;
-
-  // TODO define + for Bags
 };
 
 #endif
