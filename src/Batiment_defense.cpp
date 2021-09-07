6f132be386f2a99798ae528e78d788d7c25354d3
Hans <Hanssoun@gmail.com>
2021-09-07 18:44:43 +0200

    Creation tourelle

--- src/Batiment_defense.cpp
+++ src/Batiment_defense.cpp
@@ -1,6 +1,12 @@
 #include "Batiment_defense.h"
 
-Batiment_defense::Batiment_defense(std::string nom) : Batiment(nom) {}
+Batiment_defense::Batiment_defense(std::string type) : Batiment(type)
+ {
+     if type == "Tourelle"
+     {
+         m_nbDegats = 5;
+     }
+ }
 
 void Batiment_defense::attaquer(Cible &cible)
 {
